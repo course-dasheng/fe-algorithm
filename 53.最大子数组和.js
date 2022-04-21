b38eb1eb91af9f7ec6b702909ae1793ff108a4cd
花果山大圣 <316783812@qq.com>
2022-04-21 16:14:10 +0800

    53

---
+++ 53.最大子数组和.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=javascript
+ *
+ * [53] 最大子数组和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var maxSubArray = function(nums) {
+  let dp = new Array(nums.length).fill(0)
+  dp[0] = nums[0]
+  let ret = nums[0]
+  for(let i=1;i<nums.length;i++){
+    dp[i] = Math.max(dp[i-1]+nums[i], nums[i])
+    ret = Math.max(dp[i],ret)
+  }
+  return ret
+  // dp[i】i之前的数组，最大连续和是dp[i]
+ }
+var maxSubArray1 = function(nums) {
+  let ret = -Infinity
+  let count = 0
+  for(let i=0;i<nums.length;i++){
+    count+=nums[i]
+    if(count>ret){
+      ret = count
+    }
+    if(count<0){
+      count=0
+    }
+  }
+  return ret
+};
+// @lc code=end
+
