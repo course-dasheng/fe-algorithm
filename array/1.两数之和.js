0341cc44b97ce652bfcec08c6aeccfa0ff3128a4
花果山大圣 <316783812@qq.com>
2022-05-25 14:03:03 +0800

    数组相关的算法

--- array/1.两数之和.js
+++ array/1.两数之和.js
@@ -20,7 +20,6 @@ var twoSum = function(nums, target) {
   }
 }
 
-
   // for(let i=0;i<nums.length;i++){
   //     for(let j=0;j<nums.length;j++){
   //         if(nums[i]+nums[j]===target && i!==j){
