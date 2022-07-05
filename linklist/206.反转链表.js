df2379e3b1319193d517bc9a34df18de6b6d22d3
花果山大圣 <316783812@qq.com>
2022-07-05 10:01:23 +0800

    Update 206.反转链表.js

--- linklist/206.反转链表.js
+++ linklist/206.反转链表.js
@@ -36,10 +36,10 @@ var reverseList = function(head) {
     // prev 上一个遍历的cur 也就是上一个节点
     [cur.next,prev,cur] = [prev,cur,cur.next]
 
-    let next = cur.next
-    cur.next = prev
-    prev = cur //缓存上一次遍历的current
-    cur = next //游标向下走
+    // let next = cur.next
+    // cur.next = prev
+    // prev = cur //缓存上一次遍历的current
+    // cur = next //游标向下走
   }
   return prev
 };
