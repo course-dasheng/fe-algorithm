6d568499e0c915b626c770971a645ef8389f1956
花果山大圣 <316783812@qq.com>
2022-07-05 09:52:23 +0800

    Update 206.反转链表.js

--- linklist/206.反转链表.js
+++ linklist/206.反转链表.js
@@ -16,6 +16,16 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+// cur
+// head-->b-->c
+
+// b.next = c 取消
+// b.next = head  (prev获取遍历的上一个节点)
+
+// c-->b-->head
+
+// prev->cur->next
+
 var reverseList = function(head) {
   if(!head || !head.next){
     return head
@@ -23,10 +33,13 @@ var reverseList = function(head) {
   let prev = null
   let cur = head
   while(cur){
+    // prev 上一个遍历的cur 也就是上一个节点
+    [cur.next,prev,cur] = [prev,cur,cur.next]
+
     let next = cur.next
     cur.next = prev
-    prev = cur
-    cur = next
+    prev = cur //缓存上一次遍历的current
+    cur = next //游标向下走
   }
   return prev
 };
