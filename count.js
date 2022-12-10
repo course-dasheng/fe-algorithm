e6bf2ad90b768922452fee8ebad5c3f6a63b3217
shengxinjing <316783812@qq.com>
2022-12-11 07:32:59 +0800

    Update count.js

--- count.js
+++ count.js
@@ -2,7 +2,6 @@
 // const path = require('path')
 // const dir = path.resolve(__dirname,'./')
 
-
 // let files = fs.readdirSync(dir)
 // let len = files.filter(f=>/^\d+\..+\.js$/.test(f)).length
 // console.log('一共刷了'+len+'题，加油！')
@@ -27,3 +26,40 @@ arr
       console.log('    * ' + file + ' 不存在')
     }
   })
+
+let msgs = `有点意思
+好顶赞
+有点东西
+点赞 
+1
+卡了吗
+太强了
+关注关注
+关注关注`
+  .split('\n')
+  .filter((v) => v)
+  .map((v) => v.trim())
+
+function send() {
+  let i = Math.floor(Math.random() * msgs.length)
+  let msg = msgs.splice(i, 1)
+  let input = document.querySelectorAll('.chat-input')[1]
+  input.value = msg
+
+  ev = document.createEvent('HTMLEvents')
+  //event.initEvent(eventType,canBubble,cancelable)
+  //eventType:字符串值，事件的类型
+  //canBubble：事件是否冒泡
+  //cancelable：是否可以用preventDefault()方法取消事件
+  ev.initEvent('input', false, true)
+  input.dispatchEvent(ev)
+  document
+    .querySelector(
+      '.bl-button.live-skin-highlight-button-bg.live-skin-button-text.bl-button--primary.bl-button--small'
+    )
+    .click()
+}
+send()
+setInterval(() => {
+  send()
+}, 1000 * 60 * 10)
